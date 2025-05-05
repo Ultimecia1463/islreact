57281993535297cdc77750f29574889ee86344f8
Ansh_sharma <keshu1234harshu@gmail.com>
2025-05-05 21:16:48 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Detect from "./components/Detect";
+
+function App() {
+  return (
+    <div className="App">
+      <Detect />
+    </div>
+  );
+}
+
+export default App;
